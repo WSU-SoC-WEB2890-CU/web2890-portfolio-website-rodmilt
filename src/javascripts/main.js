a673e30403475be3c511ac6c18997d9f1ba9d91d
rodmilt <miltonrodriguez@mail.weber.edu>
2023-11-28 18:43:06 -0700

    First commit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,10 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+  
